31b69a25b6978f567241302295efee708032b778
vikkitorry <vikkitorry6716@gmail.com>
2023-10-29 19:02:27 +0700

    refactor: remove comments

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,4 @@
-// import { Navbar } from '../components/widgets/Navbar/Navbar';
 import React, { Component, Suspense } from 'react';
-// import AppRouter from './providers/router/AppRouter';
 import { classNames } from '../utils/libs/classNames/classNames';
 import { MainPage } from '../pages/main/MainPage';
 
@@ -9,9 +7,7 @@ class App extends Component {
     return (
       <div className={classNames('app', {}, [])}>
         <Suspense fallback="">
-          {/* <Navbar /> */}
           <div className="content">
-            {/* <AppRouter /> */}
             <MainPage />
           </div>
         </Suspense>
--- src/utils/constants/Constants.ts
+++ src/utils/constants/Constants.ts
@@ -1,1 +1,1 @@
-export const SEARCH_LOCALSTORAGE_KEY = 'react-rss-vikkitorry-version.1.55557';
+export const SEARCH_LOCALSTORAGE_KEY = 'react-rss-vikkitorry-version.1.1';
