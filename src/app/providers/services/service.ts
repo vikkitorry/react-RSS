8bfd53f2dcaa3a278d4a03f7fce1a1a8123e63cc
vikkitorry <vikkitorry6716@gmail.com>
2023-11-02 19:23:54 +0700

    fix: change api

--- src/app/providers/services/service.ts
+++ src/app/providers/services/service.ts
@@ -1,61 +1,80 @@
 import {
-  AllCharacterSchema,
-  BASE_URL_PAGE,
-  NAME_ENDPOINT,
+  BASE_URL,
+  id,
+  jsonrpc,
+  METHOD,
+  Response,
+  defaultPageSize,
+  lang,
+  RequestAllShows,
+  RequestShow,
+  ShowSchema,
+  ResponseShow,
 } from './types/serviceTypes';
 import { ERROR_MESSAGE } from './types/serviceTypes';
 import { SEARCH_LOCALSTORAGE_KEY } from '../../../utils/constants/Constants';
 
-//it's not a react component, it's a part of service requests
-//it's a best practise to do it in class, I don't want to change it in functions
 export default class Service {
-  // static async getCharacter(
-  //   endpoint: string = ''
-  // ): Promise<AllCharacterSchema> {
-  //   try {
-  //     localStorage.setItem(SEARCH_LOCALSTORAGE_KEY, endpoint);
-  //     const resp = await fetch(`${BASE_URL_NAME}${endpoint}`);
-  //     if (!resp.ok) {
-  //       throw new Error();
-  //     }
-  //     const characters = (await resp.json()) as AllCharacterSchema;
-  //     return characters;
-  //   } catch (err) {
-  //     throw new Error(ERROR_MESSAGE);
-  //   }
-  // }
-
   static async getPage(
     page: number = 1,
-    name: string = ''
-  ): Promise<AllCharacterSchema> {
+    search: string = '',
+    pageSize: number = defaultPageSize
+  ): Promise<ShowSchema[]> {
+    const body: RequestAllShows = {
+      jsonrpc,
+      method: METHOD.getAllShows,
+      params: {
+        search: {
+          query: search,
+        },
+        page,
+        pageSize,
+      },
+      id,
+    };
     try {
-      localStorage.setItem(SEARCH_LOCALSTORAGE_KEY, name);
-      const resp = await fetch(
-        `${BASE_URL_PAGE}${page}${NAME_ENDPOINT}${name}`
-      );
-      if (!resp.ok) {
+      localStorage.setItem(SEARCH_LOCALSTORAGE_KEY, search);
+      const resp = await fetch(BASE_URL, {
+        method: METHOD.post,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept-Language': lang,
+        },
+        body: JSON.stringify(body),
+      });
+      const results: Response = await resp.json();
+      if (!results.result.length) {
         throw new Error();
       }
-      const characters = (await resp.json()) as AllCharacterSchema;
-      return characters;
+      return results.result;
+    } catch (err) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+
+  static async getShow(showId: number = 0): Promise<ShowSchema> {
+    const body: RequestShow = {
+      jsonrpc,
+      method: METHOD.getShow,
+      params: {
+        showId,
+        withEpisodes: true,
+      },
+      id,
+    };
+    try {
+      const resp = await fetch(BASE_URL, {
+        method: METHOD.post,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept-Language': lang,
+        },
+        body: JSON.stringify(body),
+      });
+      const res: ResponseShow = await resp.json();
+      return res.result;
     } catch (err) {
       throw new Error(ERROR_MESSAGE);
     }
   }
 }
-
-// export const getPage = async (
-//   url: string | null
-// ): Promise<AllCharacterSchema> => {
-//   try {
-//     const resp = await fetch(`${url}`);
-//     if (!resp.ok) {
-//       throw new Error();
-//     }
-//     const characters = (await resp.json()) as AllCharacterSchema;
-//     return characters;
-//   } catch (err) {
-//     throw new Error(ERROR_MESSAGE);
-//   }
-// };
--- src/app/providers/services/types/serviceTypes.ts
+++ src/app/providers/services/types/serviceTypes.ts
@@ -1,27 +1,60 @@
-export type CharacterSchema = {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  image: string;
-};
-
-export type ResponseInfo = {
-  count: number;
-  pages: number;
-  next: null | string;
-  prev: null | string;
-};
-
-export type AllCharacterSchema = {
-  info: ResponseInfo;
-  results: CharacterSchema[];
-};
-
-export const BASE_URL_NAME = 'https://rickandmortyapi.com/api/character/?name=';
-export const BASE_URL_PAGE = 'https://rickandmortyapi.com/api/character/?page=';
-export const NAME_ENDPOINT = '&name=';
-export const defaultPage = '1';
 export const ERROR_MESSAGE = 'There is nothing here';
+
+export const BASE_URL = 'https://api.myshows.me/v2/rpc/';
+export const defaultPageSize = 20;
+export const lang = 'en';
+export const id = 1;
+export const jsonrpc = '2.0';
+export enum METHOD {
+  getAllShows = 'shows.Get',
+  getShow = 'shows.GetById',
+  post = 'POST',
+}
+
+export type Response = {
+  jsonrpc: string;
+  result: ShowSchema[];
+};
+
+export type ResponseShow = {
+  jsonrpc: string;
+  result: ShowSchema;
+};
+
+export type RequestAllShows = {
+  jsonrpc: string;
+  method: string;
+  params: {
+    search: {
+      query: string;
+    };
+    page?: number;
+    pageSize?: number;
+  };
+  id: number;
+};
+
+export type RequestShow = {
+  jsonrpc: string;
+  method: string;
+  params: {
+    showId: number;
+    withEpisodes: boolean;
+  };
+  id: number;
+};
+
+export type ShowSchema = {
+  id?: number;
+  title?: string;
+  status?: string;
+  totalSeasons?: number;
+  year?: number;
+  watching?: number;
+  voted?: number;
+  rating?: number;
+  image?: string;
+  onlineCount?: number;
+  promoUrl?: string;
+  category?: string;
+};
